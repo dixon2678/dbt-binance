eaf5cbf3a45d19a8b46c7649c2ac3148b51e3b35
Dixon <81808129+dixon2678@users.noreply.github.com>
2022-07-06 22:44:54 +0700

    Update fact_overall_coins_with_sentiment_data.sql

--- binance/models/example/fact_overall_coins_with_sentiment_data.sql
+++ binance/models/example/fact_overall_coins_with_sentiment_data.sql
@@ -5,14 +5,14 @@ WITH cmc AS(
 ),
 
 a AS(
-SELECT bn.symbol AS ticker, cm.symbol AS coin, cm.name AS coin_fullname, * EXCEPT(symbol)
+SELECT bn.symbol AS ticker, cm.symbol AS coin, cm.name AS coin_fullname, cm.date_added, * EXCEPT(symbol, date_added)
 FROM `final-347314.main.binance_api` bn
 LEFT JOIN cmc cm
 ON bn.symbol LIKE CONCAT(cm.symbol, '%')
 ),
 
 b AS(
-SELECT a.*, gc.announcement, gc.category, gc.date_added, gc.description, gc.subreddit, gc.tags, gc.technical_doc, gc.twitter, gc.website, gc.symbol as gc_symbol
+SELECT a.*, gc.announcement, gc.category, gc.description, gc.subreddit, gc.tags, gc.technical_doc, gc.twitter, gc.website, gc.symbol as gc_symbol
 FROM a
 LEFT JOIN `final-347314.main.gcs_coins` gc
 ON a.coin = gc.symbol
