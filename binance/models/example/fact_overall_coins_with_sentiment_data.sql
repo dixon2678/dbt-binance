5a330b48776081548815fc521c2adfa952a6bbea
Dixon <81808129+dixon2678@users.noreply.github.com>
2022-07-06 22:38:11 +0700

    Update fact_overall_coins_with_sentiment_data.sql

--- binance/models/example/fact_overall_coins_with_sentiment_data.sql
+++ binance/models/example/fact_overall_coins_with_sentiment_data.sql
@@ -1,12 +1,18 @@
-WITH a AS(
-SELECT *, cm.symbol AS coin, cm.name AS coin_fullname
+WITH cmc AS(
+  select *
+  from `final-347314.main.cmcap_api`
+  where cmc_rank < 201
+),
+
+a AS(
+SELECT bn.symbol AS ticker, cm.symbol AS coin, cm.name AS coin_fullname, * EXCEPT(symbol)
 FROM `final-347314.main.binance_api` bn
-LEFT JOIN `final-347314.main.cmcap_api` cm
+LEFT JOIN cmc cm
 ON bn.symbol LIKE CONCAT(cm.symbol, '%')
 ),
 
 b AS(
-SELECT a.*, gc.announcement, gc.category, gc.date_added, gc.description, gc.subreddit, gc.tags, gc.technical_doc, gc.twitter, gc.website
+SELECT a.*, gc.announcement, gc.category, gc.date_added, gc.description, gc.subreddit, gc.tags, gc.technical_doc, gc.twitter, gc.website, gc.symbol as gc_symbol
 FROM a
 LEFT JOIN `final-347314.main.gcs_coins` gc
 ON a.coin = gc.symbol
@@ -17,4 +23,3 @@ SELECT *
 FROM b
 LEFT JOIN `final-347314.main.dim_sentiment_data` sent
 ON b.coin_fullname = sent.assc_coin
-WHERE coin = 'DOT'
